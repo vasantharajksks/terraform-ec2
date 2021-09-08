1243c1cd57a120b68bbdb2532697229fef8f1059
vasantharajksks <90173105+vasantharajksks@users.noreply.github.com>
2021-09-08 13:02:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "Instance1" {
   ami = var.ami_id
   key_name = var.key_name
   instance_type = var.instance_type
+  associate_public_ip_address = "true"
   subnet_id = var.subnet_id
   security_groups= ["${var.security_group}"]
   tags= {
