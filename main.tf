7242c5014270b9c5019e023bc4bebebc134330de
vasantharajksks <90173105+vasantharajksks@users.noreply.github.com>
2021-09-08 12:52:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,44 +2,12 @@ provider "aws" {
   region = var.aws_region
 }
 
-
-#Create security group with firewall rules
-resource "aws_security_group" "security_jenkins_port" {
-  name        = "security_jenkins_port"
-  description = "security group for jenkins"
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- # outbound from jenkis server
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags= {
-    Name = "security_jenkins_port"
-  }
-}
-
 resource "aws_instance" "Instance1" {
-  ami           = "ami-0b9064170e32bde34"
+  ami = var.ami_id
   key_name = var.key_name
   instance_type = var.instance_type
-  security_groups= [ "security_jenkins_port"]
+  subnet_id = var.subnet_id
+  security_groups= ["${var.security_group}"]
   tags= {
     Name = "jenkins_instance"
   }
@@ -47,5 +15,5 @@ resource "aws_instance" "Instance1" {
 }
 
 output "jenkins_endpoint" {
-  value = formatlist("http://%s:%s/", aws_instance.JenkinsEC2.*.public_ip, "8080")
+  value = formatlist("http://%s:%s/", aws_instance.Instance1.*.public_ip, "8080")
 }
