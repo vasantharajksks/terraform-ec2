7d720c274a20585ad494106b21d7bc0b2343e08c
vasantharajksks <90173105+vasantharajksks@users.noreply.github.com>
2021-09-08 13:06:50 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo yum install -y jenkins java-11-openjdk-devel
+sudo yum install -y java-11-openjdk-devel
 sudo yum upgrade -y
 sudo yum install jenkins -y`
 sudo systemctl start jenkins
