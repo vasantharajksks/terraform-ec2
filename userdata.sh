76d4b6027f9724cbc0237dac1808d2f02537abe1
vasantharajksks <90173105+vasantharajksks@users.noreply.github.com>
2021-09-08 13:42:57 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo yum install -y java-11-openjdk-devel
-sudo yum upgrade -y
+sudo yum install java-1.8.0-openjdk-devel
+curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 sudo yum install jenkins -y`
 sudo systemctl start jenkins
