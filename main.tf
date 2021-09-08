d8f97df9bf6afbffe12da693244019033bb61a36
vasantharajksks <90173105+vasantharajksks@users.noreply.github.com>
2021-09-08 14:31:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,44 @@ provider "aws" {
   region = var.aws_region
 }
 
+#Create security group with firewall rules
+resource "aws_security_group" "security_jenkins_port" {
+  name        = "security_jenkins_port"
+  description = "security group for jenkins"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ # outbound from jenkis server
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags= {
+    Name = "security_jenkins_port"
+  }
+}
+
 resource "aws_instance" "Instance1" {
   ami = var.ami_id
   key_name = var.key_name
   instance_type = var.instance_type
   associate_public_ip_address = "true"
-  subnet_id = var.subnet_id
-  security_groups= ["${var.security_group}"]
+  security_groups= [ "security_jenkins_port"]
   tags= {
     Name = "jenkins_instance"
   }
